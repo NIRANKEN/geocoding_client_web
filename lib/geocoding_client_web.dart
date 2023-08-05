e4598ffadcfb4d414bf758de66f90a4b0a92f077
NIRANKEN <nirank.8192cpjpyjs@gmail.com>
2023-08-05 17:07:46 +0900

    first commit

---
+++ lib/geocoding_client_web.dart
@@ -0,0 +1,68 @@
+library geocoding_client_web;
+
+import 'dart:math';
+
+import 'package:flutter/cupertino.dart';
+import 'package:geocoding_client_interface/geocoding_client_interface.dart';
+import 'package:geocoding_client_interface/not_found_geocoding_api_place.dart';
+import 'package:geocoding_client_interface/place_mark.dart';
+import 'package:google_maps/google_maps.dart' as gmap_web;
+import 'package:google_maps_flutter_platform_interface/google_maps_flutter_platform_interface.dart';
+import 'package:logger/logger.dart';
+
+final logger = Logger(printer: PrettyPrinter());
+
+class GeocodingClientWeb implements GeocodingClient {
+  GeocodingClientWeb({this.searchResultLimit = 5});
+  @override
+  final int searchResultLimit;
+  gmap_web.Geocoder geocoder = gmap_web.Geocoder();
+
+  @visibleForTesting
+  GeocodingClientWeb.withMockGeocoder(
+      {this.searchResultLimit = 5, required this.geocoder});
+
+  @override
+  Future<List<PlaceMark>> getGeocode(String searchText) async {
+    var geocoderRequest = gmap_web.GeocoderRequest()
+      ..address = searchText
+      ..region = "jp";
+    var response = await geocoder.geocode(geocoderRequest);
+    if (response.results != null && response.results!.isNotEmpty) {
+      return response.results!
+          .sublist(0, min(searchResultLimit, response.results!.length))
+          .map((result) {
+        String address = result?.formattedAddress ?? "";
+        var lat = result?.geometry?.location?.lat;
+        var lng = result?.geometry?.location?.lng;
+        if (lat == null || lng == null) {
+          return const PlaceMark(
+              name: "dummy", address: "dummy", latLng: LatLng(0, 0));
+        }
+        var latLng = LatLng(lat.toDouble(), lng.toDouble());
+        return PlaceMark(name: searchText, address: address, latLng: latLng);
+      }).toList();
+    } else {
+      throw NotFoundGeocodingApiPlace();
+    }
+  }
+
+  @override
+  Future<PlaceMark> getPlaceMark(LatLng latLng) async {
+    var latLngForWeb = gmap_web.LatLng(latLng.latitude, latLng.longitude);
+    var geocoderRequest = gmap_web.GeocoderRequest()
+      ..region = "jp"
+      ..location = latLngForWeb;
+    var response = await geocoder.geocode(geocoderRequest);
+    if (response.results != null && response.results!.isNotEmpty) {
+      gmap_web.GeocoderResult? result = response.results!.first;
+      return PlaceMark(
+        name: "名前を変更してね",
+        address: result?.formattedAddress ?? "",
+        latLng: latLng,
+      );
+    } else {
+      throw NotFoundGeocodingApiPlace();
+    }
+  }
+}
